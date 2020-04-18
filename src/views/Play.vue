4c9e6cf72878e4198e8f81be03cb319231ef1860
Camille Briand <camille.briand@efrei.net>
2020-04-18 20:55:49 +0200

    Add Play view

---
+++ src/views/Play.vue
@@ -0,0 +1,19 @@
+<template>
+  <div>
+    <h1 class="display-4">Play view</h1>
+
+    <v-btn to="/" color="accent">
+      Go back to home
+    </v-btn>
+  </div>
+</template>
+
+<script>
+export default {
+
+}
+</script>
+
+<style>
+
+</style>
