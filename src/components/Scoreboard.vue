73441e3f758b0789c8340d9555eb7a42f3bf4442
Camille Briand <camille.briand@efrei.net>
2020-04-18 23:16:00 +0200

    Finish scoreboard implementation

--- src/components/Scoreboard.vue
+++ src/components/Scoreboard.vue
@@ -1,5 +1,16 @@
 <template>
-  <div>Scoreboard Component</div>
+  <v-card elevation="10" class="accent fill-height px-12 pt-6 white--text">
+    <h3 class="headline mb-5 text-underline">Scoreboard</h3>
+    <div v-for="(item, idx) of scoreboard" :key="idx" class="d-flex flex-row justify-space-between primary--text">
+      <span class="body-1 font-weight-medium"
+            :class="{'font-italic': item.found, 'white--text': item.found}">
+            {{ item.player }}
+      </span>
+      <span class="body-1 font-weight-black"
+            :class="{'white--text': item.found}">
+      {{ item.score }}</span>
+    </div>
+  </v-card>
 </template>
 
 <script>
@@ -8,8 +19,18 @@ export default {
 
   computed: {
     scoreboard () {
-      return this.$store.state.scorebord
+      return this.$store.state.scoreboard
+    },
+
+    successTextColor () {
+      return this.$vuetify.theme.themes.light.success
     }
   }
 }
 </script>
+
+<style scoped>
+.text-underline {
+  text-decoration: underline;
+}
+</style>
