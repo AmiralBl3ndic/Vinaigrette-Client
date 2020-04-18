90c028b038e44788613c052fb97ec5661e27836d
Camille Briand <camille.briand@efrei.net>
2020-04-18 12:21:37 +0200

    Start VueSocketIO config

--- src/main.js
+++ src/main.js
@@ -1,4 +1,5 @@
 import Vue from 'vue'
+import VueSocketIO from 'vue-socket.io'
 import App from './App.vue'
 import router from './router'
 import vuetify from './plugins/vuetify'
@@ -6,6 +7,11 @@ import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
 
 Vue.config.productionTip = false
+
+Vue.use(new VueSocketIO({
+  debug: true,
+  connection: 'http://localhost:1664'
+}))
 
 new Vue({
   router,
