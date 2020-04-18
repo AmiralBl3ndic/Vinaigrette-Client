2c5cdb55b21f556fece5f8da6b70d4ca55d2d0cf
Camille Briand <camille.briand@efrei.net>
2020-04-18 17:29:14 +0200

    Add Vuex code to update rooms list

--- src/store/index.js
+++ src/store/index.js
@@ -5,10 +5,17 @@ Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    rooms: []
   },
   mutations: {
+    SET_ROOMS_LIST (state, payload) {
+      state.rooms = payload
+    }
   },
   actions: {
+    SOCKET_rooms_list_update ({ commit }, payload) {
+      commit('SET_ROOMS_LIST', payload.roomNames)
+    }
   },
   modules: {
   }
