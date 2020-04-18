7480f0274d833b929f60e5d43ba09dd2a9074b0c
Camille Briand <camille.briand@efrei.net>
2020-04-18 20:05:47 +0200

    Finish RoomSelector component

--- src/components/RoomSelector.vue
+++ src/components/RoomSelector.vue
@@ -6,9 +6,29 @@
 
     <v-container class="mx-12">
       <v-divider class="my-5" />
-      <div v-for="(room, id) of rooms" :key="id" class="px-15" @click.stop="handleJoinRoom(room)">
+      <div v-for="(room, id) of rooms" :key="id" class="px-15 room-link" @click.stop="handleJoinRoom(room)">
         <span class="headline">{{ room }}</span>
       </div>
+
+      <v-form ref="createRoomForm" @submit.prevent="handleCreateRoom" class="mt-5">
+        <v-row>
+          <v-col md="8" offset-md="2" class="d-flex flex-column align-center">
+            <h2 class="display-1 my-4">Create a new room</h2>
+          </v-col>
+        </v-row>
+
+        <v-row align="center">
+          <v-col cols="7" offset="1">
+            <v-text-field v-model="newRoomName" placeholder="Room name" outlined hide-details />
+          </v-col>
+
+          <v-col cols="3" offset="0">
+            <v-btn block type="submit" color="accent" class="py-7">
+              Create
+            </v-btn>
+          </v-col>
+        </v-row>
+      </v-form>
     </v-container>
   </div>
 </template>
@@ -16,6 +36,12 @@
 <script>
 export default {
   name: 'RoomSelector',
+
+  data () {
+    return {
+      newRoomName: ''
+    }
+  },
 
   computed: {
     rooms () {
@@ -26,29 +52,52 @@ export default {
   methods: {
     handleJoinRoom (roomName) {
       this.$socket.emit('join_room', { roomName })
-    }
-  },
+    },
 
-  sockets: {
-    join_room_error (data) {
+    handleCreateRoom () {
+      if (this.newRoomName) {
+        this.$socket.emit('create_room', { roomName: this.newRoomName })
+        this.$refs.createRoomForm.reset()
+      }
+    },
+
+    showErrorAlert (title, textContent) {
       this.$swal({
         icon: 'error',
-        titleText: 'Could not join room',
-        text: data.error,
+        titleText: title,
+        text: textContent,
         customClass: {
           container: 'primary'
         },
         confirmButtonColor: this.$vuetify.theme.themes.light.secondary
       })
+    }
+  },
+
+  sockets: {
+    join_room_error (data) {
+      this.showErrorAlert('Could not join room', data.error)
+    },
+
+    create_room_error (data) {
+      this.showErrorAlert('Could not create room', data.error)
     },
 
     join_room_success (data) {
-      console.log('Room joining success!', data)
+      // TODO: dispatch room name to store
+      this.$router.push('/play')
+    },
+
+    create_room_success (data) {
+      // TODO: dispatch room name to store
       this.$router.push('/play')
     }
   }
 }
 </script>
 
-<style>
+<style scoped>
+.room-link:hover {
+  cursor: pointer;
+}
 </style>
