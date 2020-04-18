c504b681a51529db22fb5c5f29330811fc0b2ca2
Camille Briand <camille.briand@efrei.net>
2020-04-18 23:16:18 +0200

    Position scoreboard on right side of Play view

--- src/views/Play.vue
+++ src/views/Play.vue
@@ -1,23 +1,30 @@
 <template>
-  <div>
-    <h1 class="display-4">Play view</h1>
+  <div class="fill-height d-flex">
 
-    <v-btn to="/" color="accent">
-      Go back to home
-    </v-btn>
+    <v-container>
+      <h1 class="display-4">Play view</h1>
+
+      <v-btn to="/" color="accent">
+        Go back to home
+      </v-btn>
+    </v-container>
+
+    <Scoreboard style="flex-basis: 300px" />
   </div>
 </template>
 
 <script>
+import Scoreboard from '@/components/Scoreboard.vue'
+
 export default {
   name: 'Play',
+
+  components: {
+    Scoreboard
+  },
 
   beforeDestroy () {
     this.$socket.emit('leave_room', { roomName: this.$store.state.currentRoom })
   }
 }
 </script>
-
-<style>
-
-</style>
