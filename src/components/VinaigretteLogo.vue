4f2bd28c1c9f270fab5fc17d7cc3faed19783628
Camille Briand <camille.briand@efrei.net>
2020-04-18 15:29:35 +0200

    Add a component to hold our logo

---
+++ src/components/VinaigretteLogo.vue
@@ -0,0 +1,46 @@
+<template>
+  <v-img
+    :src="image"
+    :height="size"
+    :width="size"
+    contain
+  />
+</template>
+
+<script>
+const logo = require('../assets/vinaigrette-logo.svg')
+const transparentLogo = require('../assets/vinaigrette-logo-transparent.svg')
+
+export default {
+  name: 'VinaigretteLogo',
+
+  props: {
+    size: {
+      type: Number,
+      required: true
+    },
+    transparent: {
+      type: Boolean,
+      default: false,
+      required: false
+    },
+    class: {
+      default: ''
+    }
+  },
+
+  beforeMount () {
+    console.log('Attributes:', this.$attrs)
+  },
+
+  computed: {
+    image () {
+      return this.$props.transparent ? transparentLogo : logo
+    },
+
+    classes () {
+      return this.$attrs.class
+    }
+  }
+}
+</script>
