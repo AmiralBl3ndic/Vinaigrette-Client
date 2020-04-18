f4c98af7e60e917c2f1e229df3ce7532bec3eb12
Camille Briand <camille.briand@efrei.net>
2020-04-18 12:22:11 +0200

    Setup project to use Vuex

--- src/main.js
+++ src/main.js
@@ -5,6 +5,7 @@ import router from './router'
 import vuetify from './plugins/vuetify'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
+import store from './store'
 
 Vue.config.productionTip = false
 
@@ -16,5 +17,6 @@ Vue.use(new VueSocketIO({
 new Vue({
   router,
   vuetify,
+  store,
   render: h => h(App)
 }).$mount('#app')
