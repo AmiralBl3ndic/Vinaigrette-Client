42ef88ff636ed2bf83fb58f393f8dfef8cd89c69
Camille Briand <camille.briand@efrei.net>
2020-04-18 20:56:58 +0200

    Make router serve Play view

--- src/router/index.js
+++ src/router/index.js
@@ -2,6 +2,7 @@ import Vue from 'vue'
 import VueRouter from 'vue-router'
 import PageNotFound from '@/components/PageNotFound.vue'
 import Home from '@/views/Home.vue'
+import Play from '@/views/Play.vue'
 
 Vue.use(VueRouter)
 
@@ -10,6 +11,11 @@ const routes = [
     path: '/',
     name: 'Home',
     component: Home
+  },
+  {
+    path: '/play',
+    name: 'Play',
+    component: Play
   },
   { // Handle 404 errors
     path: '*',
