2844bf637b70f03b557e9aea48afad2a0fad9723
Camille Briand <camille.briand@efrei.net>
2020-04-19 00:55:21 +0200

    Import and configure VueDropzone2

--- src/main.js
+++ src/main.js
@@ -1,18 +1,18 @@
-import axios from 'axios'
-
 import Vue from 'vue'
 import Vuex from 'vuex'
 import VueSocketIO from 'vue-socket.io'
 import VueAxios from 'vue-axios'
 import VueSweetAlert2 from 'vue-sweetalert2'
-import App from './App.vue'
-import router from './router'
-import vuetify from './plugins/vuetify'
+import axios from 'axios'
 import 'roboto-fontface/css/roboto/roboto-fontface.css'
 import '@fortawesome/fontawesome-free/css/all.css'
-import store from './store'
-
 import '@sweetalert2/theme-dark/dark.min.css'
+import 'vue2-dropzone/dist/vue2Dropzone.css'
+
+import vuetify from './plugins/vuetify'
+import router from './router'
+import store from './store'
+import App from './App.vue'
 
 import { serverUrl } from '@/config'
 
