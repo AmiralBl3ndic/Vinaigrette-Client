d3af343f1843489c0017cc54575b5f149d1aab6b
Camille Briand <camille.briand@efrei.net>
2020-04-19 13:27:20 +0200

    Implement QuoteSauce component

--- src/components/QuoteSauce.vue
+++ src/components/QuoteSauce.vue
@@ -1,9 +1,21 @@
 <template>
-  <div>Quote sauce</div>
+  <v-container>
+    <h3 class="headline light--text text-center">What's the sauce for this quote?</h3>
+
+    <blockquote class="display-2 light--text mt-10 text-center">
+      “{{ quote }}”
+    </blockquote>
+  </v-container>
 </template>
 
 <script>
 export default {
-  name: 'QuoteSauce'
+  name: 'QuoteSauce',
+
+  computed: {
+    quote () {
+      return this.$store.state.currentSauce.quote
+    }
+  }
 }
 </script>
