5a8ea40df603d85155e0130b21b5a5f5af710a17
Camille Briand <camille.briand@efrei.net>
2020-04-19 02:11:53 +0200

    Change layout of Contribute view

--- src/views/Contribute.vue
+++ src/views/Contribute.vue
@@ -1,10 +1,21 @@
 <template>
   <v-container>
-    <v-row>
+    <router-link to="/" class="d-flex align-center mt-5">
+      <div>
+        <v-icon large class="primary--text">fas fa-arrow-left</v-icon>
+      </div>
+      <div>
+        <span class="display-1 ml-2 primary--text no-underline">Back</span>
+      </div>
+    </router-link>
+
+    <v-row class="mt-5">
       <v-col cols="12">
         <NewQuoteSauceForm />
       </v-col>
     </v-row>
+
+    <v-divider class="my-12" />
 
     <v-row>
       <v-col cols="12">
@@ -27,3 +38,9 @@ export default {
   }
 }
 </script>
+
+<style scoped>
+a {
+  text-decoration: none;
+}
+</style>
