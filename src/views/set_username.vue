2b6ddb86be61c7860038ea2d24e2e6b75e634fa4
Elodie <e.dehache@gmail.com>
2020-04-20 12:15:24 +0200

    betise elodie

---
+++ src/views/set_username.vue
@@ -0,0 +1,15 @@
+<template>
+  <SetUsername />
+</template>
+
+<script>
+
+import Welcome from '@/components/Welcom.vue'
+
+export default {
+  name: 'Home',
+  components: {
+    Welcome
+  }
+}
+</script>
