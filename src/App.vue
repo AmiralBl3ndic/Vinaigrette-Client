f8ea57c368a1504494b93d37f44f603e210a156a
jules <jules.lagny@efrei.net>
2020-04-21 12:58:16 +0200

    add login

--- src/App.vue
+++ src/App.vue
@@ -4,21 +4,19 @@
       app
       color="primary"
       dark
-      elevation="1"
-    >
+      elevation="1">
 
       <div class="d-flex align-center">
         <VinaigretteLogo :size="40" transparent />
-
         <router-link to="/"><h1 class="ml-2 white--text">Vinaigrette</h1></router-link>
       </div>
 
-      <div class="login">
-      <button align="right" v-text="center">
-        <p>Login</p>
-      </button>
-    </div>
     </v-app-bar>
+
+    <div>
+      <input type="text" name="username" v-model="input.username" placeholder="Username"/>
+      <button type="button" v-on:click="login()">Login</button>
+    </div>
 
     <v-content class="secondary">
       <router-view></router-view>
@@ -36,8 +34,14 @@ export default {
     VinaigretteLogo
   },
 
+  methods: {
+    login () {
+      this.$socket.emit('set_username', { username: this.username })
+    }
+  },
+
   data: () => ({
-    //
+
   })
 }
 </script>
