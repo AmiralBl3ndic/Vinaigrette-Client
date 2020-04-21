1c5e084540a994fad6da11f3c001527175520ffa
jules <jules.lagny@efrei.net>
2020-04-21 16:35:49 +0200

    use SweetAlert2 popup

--- src/components/Popup.vue
+++ src/components/Popup.vue
@@ -1,19 +1,14 @@
+// example-vue-component.vue
 <template>
-  <div class="text-center">
-    <v-dialog v-model="dialog" width="500">
-      <template v-slot:activator="{ on }">
-        <v-btn class="success" dark v-on="on">Login</v-btn>
-      </template>
-      <v-card>
-        <v-card-title class="headline grey lighten-2" primary-title>Login</v-card-title>
-        <input v-model="username" placeholder="votre username">
-        <button type="button" v-on:click="login()">Login</button>
-      </v-card>
-    </v-dialog>
-  </div>
+  <button @click="showAlert">Login</button>
 </template>
+
 <script>
 export default {
-
+  methods: {
+    showAlert () {
+      this.$swal('Hello Vue world!!!')
+    }
+  }
 }
 </script>
