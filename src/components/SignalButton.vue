552833ead5d9996718bf07f395caaf62e6fc073c
Paul lepre <paul.lepre@efrei.net>
2020-04-22 11:47:19 +0200

    siganlbutton

--- src/components/SignalButton.vue
+++ src/components/SignalButton.vue
@@ -1,18 +1,23 @@
 <template>
-  <div >
-    <p>
-     <v-icon @click.stop="sendSignal">far fa-bell</v-icon>
-     Report Sauce</p>
-  </div>
+  <v-tooltip right >
+    <template v-slot:activator="{ on }">
+      <v-icon @click.stop="sendSignal" v-on="on" v-show = "show">far fa-bell</v-icon>
+    </template>
+    <span>Report Sauce</span>
+  </v-tooltip>
 </template>
 
 <script>
 export default {
   name: 'SignalButton',
 
+  props: {
+    show: Boolean
+  },
+
   methods: {
     sendSignal () {
-      this.$socket.emit('report_sauce')
+      // this.$socket.emit('report_sauce')
       this.$emit('report')
     }
   }
